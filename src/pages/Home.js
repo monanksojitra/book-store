1d61092c3a9de612ca07bc3ac936eeea000dd371
monank <sojitramonank2003@gmail.com>
2023-02-28 18:33:17 +0530

    Day

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import Titel from '../component/Titel';
+import List from '../component/List';
+// import Titel from '../component/Titel';
 
 
 class Home extends Component {
@@ -16,6 +17,7 @@ class Home extends Component {
         <h1>Home form Home.js</h1> 
         <h3>{this.state.title}</h3>
         <h3>{this.state.dis}</h3>
+        <List/>
       </div>
     )
   }
