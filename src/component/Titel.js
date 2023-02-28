a5168a7b9fcf20a40ca17a212a00c74e303bc48a
monank <sojitramonank2003@gmail.com>
2023-02-28 12:43:09 +0530

    new changes only

--- src/component/Titel.js
+++ src/component/Titel.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Titel(prope) {
   return (
     <div>
-    <h1>Titel form {prope.title} and Description {prope.dis}</h1>
+    <h1>Titel form {prope.title} and  Description {prope.dis}</h1>
     <h1>Titel form {prope.title} and Description {prope.dis}</h1>
     <h1>Titel form {prope.title} and Description {prope.dis}</h1>
   </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,18 @@ import React, { Component } from 'react'
 import Titel from '../component/Titel';
 
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {title: this.props.title,
-                        dis: this.props.dis};
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: this.props.title,
+      dis: this.props.dis
+    };
+  }
   render() {
     return (
       <div>
         <h1>Home form Home.js</h1>
-        <Titel title={this.state.title} dis ={this.state.dis}/>
+        <Titel title={this.state.title} dis={this.state.dis} />
       </div>
     )
   }
