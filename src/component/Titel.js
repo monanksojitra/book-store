06be2e5eed8b737f5266241def2dfb3019a6aac5
monank <sojitramonank2003@gmail.com>
2023-02-28 11:49:25 +0530

    Day 2 work

---
+++ src/component/Titel.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export default function Titel(prope) {
+  return (
+    <div>
+    <h1>Titel form {prope.title}</h1>
+    <h1>Titel form {prope.title}</h1>
+    <h1>Titel form {prope.title}</h1>
+  </div>
+  )
+}
+
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Titel from '../component/Titel';
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home form Home.js</h1>
+        <Titel title={"this is titel form prope"}/>
+      </div>
+    )
+  }
+}
+export default Home;
