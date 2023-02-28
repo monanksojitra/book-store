fd2211fc3757ab10cf7380081697a7f6719ea07f
monank <sojitramonank2003@gmail.com>
2023-02-28 18:36:01 +0530

    Day 6 work

--- src/component/List.js
+++ src/component/List.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import Titel from './Titel'
 export default function List() {
     const list = [{ id: 1, title: "this is titel 1", description: "this is a discription form 1" }, { id: 2, title: "this is titel 2", description: "this is a discription form 2" }, { id: 3, title: "this is titel 3", description: "this is a discription form 3" }]
     const newlist = list.map(person => <h2>this is id:{person.id} and it is a {person.title} with a small dis {person.description}</h2>)
     return (
         <div>
-    {newlist}
+            <Titel list={newlist}/>
         </div>
     )
 }
--- src/component/Titel.js
+++ src/component/Titel.js
@@ -5,6 +5,7 @@ export default function Titel(prope) {
   return (
     <div>
     <h1>Titel form {prope.title} and  Description {prope.dis}</h1>
+    <h3>{prope.list}</h3>
     {/* <h1>Titel form {prope.title} and Description {prope.dis}</h1>
     <h1>Titel form {prope.title} and Description {prope.dis}</h1> */}
   </div>
