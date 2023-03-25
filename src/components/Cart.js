e28a7ee9be550fe4cce21118fc2b1eeb80d4c418
monank <sojitramonank2003@gmail.com>
2023-03-25 17:59:46 +0530

    Cart recoding

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-
+import "../css/cart.css";
 
 import Navbar from "./Navbar";
 import CartBooks from "./CartBooks";
+import { Link } from "react-router-dom";
 
 const Cart = () => {
   const [books, setBooks] = useState([]);
@@ -14,7 +15,7 @@ const Cart = () => {
   return (
     <>
       <Navbar />
-      <section className="h-100" style={{ backgroundColor: "#eee" }}>
+      {/* <section className="h-100" style={{ backgroundColor: "#eee" }}>
         <div className="container h-100 py-5">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col-10">
@@ -80,7 +81,139 @@ const Cart = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
+      <div className="container my-1">
+        <div className="wrapper wrapper-content animated fadeInRight">
+          <div className="row">
+            <div className="col-md-9">
+              <div className="ibox">
+                <div className="ibox-title d-flex">
+                  <h5 className="mx-2">Items in your cart</h5>
+                  <span className="pull-right">
+                    (<strong>{books.length}</strong>) items
+                  </span>
+                </div>
+                {!books.length == 0 ? (
+                  books.map((bookData) => (
+                    <div className="ibox-content">
+                      <div className="table-responsive">
+                        <table className="table shoping-cart-table">
+                          <tbody>
+                            <tr>
+                              <td>
+                                <div>
+                                  <img
+                                    width={90}
+                                    src={bookData.thumbnailUrl}
+                                    alt=""
+                                  />
+                                </div>
+                              </td>
+                              <td className="desc">
+                                <h3>
+                                  <Link
+                                    to="/readmore"
+                                    state={bookData}
+                                    className="text-navy"
+                                  >
+                                    {bookData.title}
+                                  </Link>
+                                </h3>
+                                <p className="small ">
+                                  It is a long established fact that a reader
+                                  will be distracted by the readable content of
+                                  a page when looking at its layout. The point
+                                  of using Lorem Ipsum is
+                                </p>
+                                <dl className="small m-b-none">
+                                  <dt>Description lists</dt>
+                                  <dd>
+                                    A description list is perfect for defining
+                                    terms.
+                                  </dd>
+                                </dl>
+                                <div className="m-t-sm">
+                                  <a href="#" className="text-muted mx-2">
+                                    <i className="fa fa-gift" /> Add gift
+                                    package
+                                  </a>
+                                  |
+                                  <a href="#" className="text-muted mx-2">
+                                    <i className="fa fa-trash" /> Remove item
+                                  </a>
+                                </div>
+                              </td>
+                             
+                              <td width={65}>
+                                <input
+                                  type="text"
+                                  className="form-control"
+                                  placeholder={1}
+                                />
+                              </td>
+                              <td>
+                                <h4>$180,00</h4>
+                              </td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <figure className="text-center mt-3">
+                    <blockquote className="blockquote">
+                      <p className="h4">Add item in cart..!</p>
+                    </blockquote>
+                  </figure>
+                )}
+              </div>
+            </div>
+            {!books.length == 0 ? (
+              <div className="col-md-3">
+                <div className="ibox">
+                  <div className="ibox-title">
+                    <h5>Cart Summary</h5>
+                  </div>
+                  <div className="ibox-content">
+                    <span>Total</span>
+                    <h2 className="font-bold">$390,00</h2>
+                    <hr />
+                    <span className="text-muted small">
+                      *For United States, France and Germany applicable sales
+                      tax will be applied
+                    </span>
+                    <div className="m-t-sm my-3">
+                      <div className="btn-group mx-2">
+                        <a href="#" className="btn btn-primary btn-sm mx-2">
+                          <i className="fa fa-shopping-cart" /> Checkout
+                        </a>
+                        <a href="#" className="btn btn-white btn-sm">
+                          Cancel
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="ibox">
+                  <div className="ibox-title">
+                    <h5>Support</h5>
+                  </div>
+                  <div className="ibox-content text-center">
+                    <h3>
+                      <i className="fa fa-phone" /> +43 100 783 001
+                    </h3>
+                    <span className="small">
+                      Please contact with us if you have any questions. We are
+                      avalible 24h.
+                    </span>
+                  </div>
+                </div>
+              </div>
+            ) : null}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
